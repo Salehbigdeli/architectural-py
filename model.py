4d55fb16f19451cd5fc8e4ec3f3ec66a9a1517f2
ali <al.ak.saleh@gmail.com>
2022-06-03 12:21:41 +0200

    first tests

---
+++ model.py
@@ -0,0 +1,66 @@
+from dataclasses import dataclass
+from datetime import date
+from typing import List, NewType, Optional, Set
+
+
+class Quantity(int):
+    pass
+
+
+class Sku(str):
+    pass
+
+
+class Reference(str):
+    pass
+
+class OrderReference(str):
+    pass
+
+
+@dataclass(frozen=True)
+class OrderLine:
+    orderid: OrderReference
+    sku: Sku
+    quantity: Quantity
+
+
+class Batch:
+    def __init__(self, ref: Reference, sku: Sku, qty: Quantity, eta: Optional[date]=None):
+        self.reference = ref
+        self.sku = sku
+        self.eta = eta
+        self._purchased_quantity = qty
+        self._allocations: Set[OrderLine] = set()
+
+    def allocate(self, orderline: OrderLine):
+        if self.can_allocate(orderline):
+            self._allocations.add(orderline)
+    
+
+    def deallocate(self, orderline: OrderLine):
+        if orderline in self._allocations:
+            self._allocations.remove(orderline)
+
+    @property
+    def allocated_quantity(self):
+        return sum(orderline.quantity for orderline in self._allocations)
+
+    @property
+    def available_quantity(self) -> Quantity:
+        return self._purchased_quantity - self.allocated_quantity
+    
+
+    def can_allocate(self, orderline: OrderLine):
+        if orderline.sku != self.sku:
+            return False
+        return self.available_quantity >= orderline.quantity
+
+    def __eq__(self, o: object) -> bool:
+        if not isinstance(o, Batch):
+            return False
+        return self.reference == o.reference
+
+    def __hash__(self) -> int:
+        return hash(self.reference)
+
--- test_model.py
+++ test_model.py
@@ -1,26 +1,53 @@
 from datetime import date, timedelta
+from typing import Tuple
 import pytest
 
-# from model import ...
+from model import OrderLine, Batch, OrderReference, Quantity, Reference, Sku
 
 today = date.today()
 tomorrow = today + timedelta(days=1)
 later = tomorrow + timedelta(days=10)
 
+def make_batch_and_line(sku: Sku, batch_qty: Quantity, line_qty: Quantity) -> Tuple[Batch, OrderLine]:
+    batch = Batch(Reference('batch-01'), sku, batch_qty)
+    line = OrderLine(OrderReference('order-001'), sku, line_qty)
+    return batch, line
+
 def test_allocating_to_a_batch_reduces_the_available_quantity():
-    pytest.fail('todo')
+    b, o = make_batch_and_line('Black-Chair', 20, 2)
+    b.allocate(o)
+    assert b.available_quantity == 18
 
 def test_can_allocate_if_available_greater_than_required():
-    pytest.fail('todo')
+    b, o = make_batch_and_line('Black-Chair', 20, 2)
+    assert b.can_allocate(o)
 
 def test_cannot_allocate_if_available_smaller_than_required():
-    pytest.fail('todo')
+    b, o = make_batch_and_line('Black-Chair', 2, 20)
+    assert b.can_allocate(o) is False
 
 def test_can_allocate_if_available_equal_to_required():
-    pytest.fail('todo')
+    b, o = make_batch_and_line('Black-Chair', 2, 2)
+    assert b.can_allocate(o)
 
-def test_prefers_warehouse_batches_to_shipments():
-    pytest.fail('todo')
+def test_cannot_allocate_if_sku_mismatch():
+    batch = Batch(Reference('batch-01'), Sku('Black-Chair'), 100)
+    different_sku_line = OrderLine(OrderReference('order-001'), Sku('Red-Chair'), 2)
+    assert batch.can_allocate(different_sku_line) is False
 
-def test_prefers_earlier_batches():
-    pytest.fail('todo')
+def test_can_only_deallocate_allocated_lines():
+    batch, unallocated_line = make_batch_and_line('Black-Chair', 20, 2)
+    batch.deallocate(unallocated_line)
+    assert batch.available_quantity == 20
+
+def test_allocation_is_idempotent():
+    batch, line = make_batch_and_line('Black-Chair', 20, 2)
+    batch.allocate(line)
+    batch.allocate(line)
+    assert batch.available_quantity == 18
+
+# def test_prefers_warehouse_batches_to_shipments():
+#     pytest.fail('todo')
+
+# def test_prefers_earlier_batches():
+#     pytest.fail('todo')
